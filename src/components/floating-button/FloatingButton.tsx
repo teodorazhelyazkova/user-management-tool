efd6e76ff5627f5403a59ace097a9360c9766612
teodorazhelyazkova <teodora.s.zhelyazkova@gmail.com>
2023-09-30 11:01:13 +0300

    create Floating Button component

---
+++ src/components/floating-button/FloatingButton.tsx
@@ -0,0 +1,28 @@
+import { FC, useMemo } from 'react';
+import cn from 'classnames';
+import { useLocation } from 'react-router-dom';
+import { DASHBOARD_PATH } from '../../constants/paths';
+import AddIcon from '@mui/icons-material/Add';
+import SettingsIcon from '@mui/icons-material/Settings';
+import styles from './FloatingButton.module.scss';
+
+export const FloatingButton: FC = () => {
+  const location = useLocation();
+  const isDashboardLocation = useMemo(
+    () => location.pathname === DASHBOARD_PATH,
+    [location.pathname]
+  );
+  const inviteUserHandler = () => {};
+
+  return (
+    <button
+      onClick={inviteUserHandler}
+      className={cn(styles.FloatingButton, {
+        [styles['FloatingButton--blue']]: isDashboardLocation,
+        [styles['FloatingButton--gray']]: !isDashboardLocation,
+      })}
+    >
+      {isDashboardLocation ? <AddIcon /> : <SettingsIcon />}
+    </button>
+  );
+};
