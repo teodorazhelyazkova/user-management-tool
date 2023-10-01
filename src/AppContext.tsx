e76f371ec5229b4d0a48bb5848e51c8e336e816b
teodorazhelyazkova <teodora.s.zhelyazkova@gmail.com>
2023-10-01 22:30:07 +0300

    fix children type in context

--- src/AppContext.tsx
+++ src/AppContext.tsx
@@ -1,4 +1,4 @@
-import { createContext, useState, useContext } from 'react';
+import React, { createContext, useState, useContext } from 'react';
 import { IUser } from './components/User/interface/User.interface';
 import data from './data/data.json';
 
@@ -12,8 +12,7 @@ interface IAppContext {
 }
 export const AppContext = createContext<IAppContext | null>(null);
 
-// eslint-disable-next-line @typescript-eslint/no-explicit-any
-export const DataProvider = ({ children }: { children: any }) => {
+export const DataProvider = ({ children }: { children: React.ReactNode }) => {
   const data = useDataProvider();
 
   return <AppContext.Provider value={data}>{children}</AppContext.Provider>;
